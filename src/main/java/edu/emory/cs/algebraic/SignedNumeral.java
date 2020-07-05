2f7c2268733472163425f9105e7c1e93ce7f16dd
Jinho Choi <jinho.choi@emory.edu>
2020-07-04 23:45:00 -0400

    Updated java_programming.md

--- src/main/java/edu/emory/cs/algebraic/SignedNumeral.java
+++ src/main/java/edu/emory/cs/algebraic/SignedNumeral.java
@@ -18,37 +18,5 @@ package edu.emory.cs.algebraic;
 /**
  * @author Jinho D. Choi
  */
-public class Number {
-//    public
-//
-
-    /**
-     * Adds `n` to this number.
-     * @param n the number to be added.
-     */
-    public void add(Number n) {}
-
-    /**
-     * Multiplies `n` to this number.
-     * @param n the number to be multiplied.
-     */
-    public void multiply(Number n) {}
-
-
-
-//    public void minus(Number a) {}
-
-//    public Number obelus(Number a) {}
-
-
-//    static public void main(String[] args) {
-//        byte a, b, c, d;
-//        a = 9;
-//        b = 6;
-//        c = (byte) (a + b);
-//        d = (byte) (b - a);
-//
-//        System.out.println(c);
-//        System.out.println(d);
-//    }
+public class SignedNumeral {
 }
