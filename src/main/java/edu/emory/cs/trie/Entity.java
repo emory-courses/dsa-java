459c57f36ffbdbfa583052bdea8eebdc484efbde
Jinho Choi <jinho.choi@emory.edu>
2020-10-14 11:55:24 -0400

    Update Quiz 5.

--- src/main/java/edu/emory/cs/trie/Entity.java
+++ src/main/java/edu/emory/cs/trie/Entity.java
@@ -13,24 +13,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package edu.emory.cs.trie;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
 
-public class TrieQuiz<T> extends Trie<T> {
+/** @author Jinho D. Choi */
+public class Entity {
+    public int begin_index;
+    public int end_index;
+    public int country_id;
+
     /**
-     * PRE: this trie contains all country names as keys and their unique IDs as values
-     * (e.g., this.get("United States") -> 0, this.get("South Korea") -> 1).
-     * @param input the input string in plain text
-     *              (e.g., "I was born in South Korea and raised in the United States").
-     * @return the list of entities (e.g., [Entity(14, 25, 1), Entity(44, 57, 0)]).
+     * @param begin_index the offset of the first character (inclusive).
+     * @param end_index the offset of the last character (exclusive).
+     * @param country_id the unique country ID.
      */
-    List<Entity> getEntities(String input) {
-        List<Entity> entities = new ArrayList<>();
-        // TODO: to be filled.
-
-        return entities;
+    public Entity(int begin_index, int end_index, int country_id) {
+        this.begin_index = begin_index;
+        this.end_index = end_index;
+        this.country_id = country_id;
     }
 }
