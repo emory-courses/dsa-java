ea2e5be82246a2b2dc309e5d7a976c458190587a
Jinho Choi <jinho.choi@emory.edu>
2020-11-11 12:10:28 -0500

    Add MSTAllHW.java

---
+++ src/main/java/edu/emory/cs/graph/span/MSTAllHW.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2020 Emory University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.emory.cs.graph.span;
+
+import edu.emory.cs.graph.Graph;
+
+import java.util.List;
+
+/** @author Jinho D. Choi */
+public class MSTAllHW implements MSTAll {
+    @Override
+    public List<SpanningTree> getMinimumSpanningTrees(Graph graph) {
+        // TODO: to be updated
+        return null;
+    }
+}
