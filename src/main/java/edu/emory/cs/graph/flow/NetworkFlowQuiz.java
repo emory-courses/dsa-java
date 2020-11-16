d9eb7a2643a657d499b2122079a70239f35b2b5f
Jinho Choi <jinho.choi@emory.edu>
2020-11-16 11:42:05 -0500

    Update NetworkFlowQuiz.java

---
+++ src/main/java/edu/emory/cs/graph/flow/NetworkFlowQuiz.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020 Emory University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.emory.cs.graph.flow;
+
+import edu.emory.cs.graph.Graph;
+import edu.emory.cs.graph.Subgraph;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/** @author Jinho D. Choi */
+public class NetworkFlowQuiz {
+    /**
+     * @param graph  Graph
+     * @param source Source vertex
+     * @param target Target vertex
+     * @return a set of all augmenting paths between the source and the target vertices.
+     */
+    private Set<Subgraph> getAugmentingPaths(Graph graph, int source, int target) {
+        // TODO: to be updated
+        return null;
+    }
+}
